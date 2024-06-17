54ed16cb587017df538b0f22245f54f2c7477e8c
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-06-17 10:13:38 +0300

    kata3

--- js/codewars/kata3.js
+++ js/codewars/kata3.js
@@ -216,4 +216,67 @@ console.log(splitAndMerge("Hello World!","."));
 console.log(splitAndMerge("Hello World!", ","));
 console.log(`\n 91 uzdv..................`);
 
+// https://www.codewars.com/kata/5763bb0af716cad8fb000580/train/javascript
+function countSquares(cuts) {
+  if (cuts === 0) {
+    return 1;
+  } else {
+    return (6 * cuts ** 2) + 2;
+  }
+}
+console.log(countSquares(5), 152);
+console.log(countSquares(16), 1538);
+console.log(countSquares(23), 3176);
+console.log(`\n 92 uzdv...............`);
+
+// https://www.codewars.com/kata/5ae62fcf252e66d44d00008e/train/javascript
+/*function expressionMatter(a, b, c) {
+  return Math.max(a + b + c, a * (b + c), a + b * c, (a + b) * c, a * b * c);
+
+}*/
+
+// kitas budas
+function expressionMatter(a, b, c) {
+    let result1 = a * (b + c)
+    let result2 = a * b * c
+    let result3 = a + b * c
+    let result4 = (a + b) * c
+    let result5 = a + b + c
+    let result6 = a * b + c
+
+    let biggest = result1
+    if (biggest < result2){
+        biggest = result2
+    } if (biggest < result3){
+        biggest = result3
+    } if (biggest < result4){
+        biggest = result4
+    } if (biggest < result5){
+        biggest = result5
+    } if (biggest < result6){
+        biggest = result6
+    }
+
+    return biggest
+}
+console.log(expressionMatter(2, 1, 2), 6);
+console.log(expressionMatter(2, 1, 1), 4);
+console.log(expressionMatter(1, 1, 1), 3);
+console.log(expressionMatter(1, 2, 3), 9);
+console.log(expressionMatter(1, 3, 1), 5);
+console.log(expressionMatter(2, 2, 2), 8);
+console.log(expressionMatter(5, 1, 3), 20);
+console.log(expressionMatter(3, 5, 7), 105);
+console.log(expressionMatter(5, 6, 1), 35);
+console.log(expressionMatter(1, 6, 1), 8);
+console.log(expressionMatter(2, 6, 1), 14);
+console.log(expressionMatter(6, 7, 1), 48);
+console.log(expressionMatter(2, 10, 3), 60);
+console.log(expressionMatter(1, 8, 3), 27);
+console.log(expressionMatter(9, 7, 2), 126);
+console.log(expressionMatter(1, 1, 10), 20);
+console.log(expressionMatter(9, 1, 1), 18);
+console.log(expressionMatter(10, 5, 6), 300);
+console.log(expressionMatter(1, 10, 1), 12);
+console.log(`\n 93 uzdv..............`);
 
