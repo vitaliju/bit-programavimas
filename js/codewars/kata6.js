2e34b266a69ace9b5cf25857be8feca8b329c386
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-08-05 08:51:07 +0300

    kata6 update

--- js/codewars/kata6.js
+++ js/codewars/kata6.js
@@ -260,3 +260,19 @@ console.log(remove("!Hi!"));
 console.log(remove("Hi! Hi!"));
 console.log(remove("Hi"));
 console.log(`\n 151 uzdv............`);
+
+// https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e/train/javascript
+function sumOfDifferences(arr) {
+    if (arr === 1) {
+        return 0;
+    }
+    const newArr = arr.sort((a, b) => b - a);
+    let sum = 0;
+    for (let i = 0; i < newArr.length - 1; i++) {
+        sum += newArr[i] - arr[i + 1];
+    }
+    return sum;
+}
+console.log(sumOfDifferences([1, 2, 10]), 9);
+console.log(sumOfDifferences([-3, -2, -1]), 2);
+console.log(`\n 151 uzdv..............`); 
