3e46e7b0b7a1427df450ee2ed452a6854e2c9f86
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-08-07 08:43:35 +0300

    file update

--- js/codewars/kata7.js
+++ js/codewars/kata7.js
@@ -1,1 +1,11 @@
 console.log(`\n 161 uzdv............`);
+
+// https://www.codewars.com/kata/580a094553bd9ec5d800007d/train/javascript
+function apple(x) {
+    return Math.pow(x, 2) > 1000
+        ? "It's hotter than the sun!!"
+        : 'Help yourself to a honeycomb Yorkie for the glovebox.';
+}
+console.log(apple('50'), 'It\'s hotter than the sun!!');
+console.log(apple(4), 'Help yourself to a honeycomb Yorkie for the glovebox.');
+console.log(`\n 162 uzdv...........`); 
