f25d0ea6393117f0a11ae677e187a46c4678d689
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-08-09 13:41:40 +0300

    file update

--- js/codewars/kata8.js
+++ js/codewars/kata8.js
@@ -59,3 +59,26 @@ console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 3), 
 console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 15), "");
 console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 0), "");
 console.log(`\n 183 uzdv.................`);
+
+// https://www.codewars.com/kata/570597e258b58f6edc00230d/train/javascript
+function array(string) {
+    return string.split(',').slice(1, -1).join(' ') || null;
+}
+// kitas budas
+/*function array(string) {
+    let arr = string.split(',');
+
+    if (arr.length < 3) {
+        return null;
+    }
+
+    return arr.slice(1, arr.length - 1).join(' ');
+}*/
+console.log(array(''), null);
+console.log(array('1'), null);
+console.log(array('A1,B2'), null);
+console.log(array('1,2,3'), '2');
+console.log(array('1,2,3,4'), '2 3');
+console.log(array('A1,B2,C3,D4,E5'), 'B2 C3 D4');
+console.log(array('A,1,23,456,78,9,Z'), '1 23 456 78 9');
+console.log(`\n 184 uzdv...................`);
