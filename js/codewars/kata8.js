54dd25bc6a0a3817d2d22e17208727b16f97317b
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-08-12 13:12:22 +0300

    kata8 update

--- js/codewars/kata8.js
+++ js/codewars/kata8.js
@@ -131,3 +131,12 @@ console.log(minMax([1, 2, 3, 4, 5]));
 console.log(minMax([2334454, 5]));
 console.log(minMax([5]));
 console.log(`\n 188 uzdv..............`);
+
+//https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/javascript
+function findAverage(arr) {
+    return arr.length > 0 ? arr.reduce((a, b) => a + b) / arr.length : 0;
+}
+console.log(findAverage([1, 1, 1]), 1);
+console.log(findAverage([1, 2, 3]), 2);
+console.log(findAverage([1, 2, 3, 4]), 2.5);
+console.log(`\ 189 uzdv...........`);
