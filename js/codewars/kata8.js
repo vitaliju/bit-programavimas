96ba658858f9d48f35f689b032e51d1510b7af3d
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-08-14 15:09:52 +0300

    kata8 update

--- js/codewars/kata8.js
+++ js/codewars/kata8.js
@@ -256,3 +256,38 @@ console.log(likes(['Jacob', 'Alex']));
 console.log(likes(['Max', 'John', 'Mark']));
 console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']));
 console.log(`\n 194 uzdv...............`);
+
+// https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/javascript
+const rps = (p1, p2) => {
+    if ((p1 === 'rock' && p2 === 'scissors')
+        || (p1 === 'scissors' && p2 === 'paper')
+        || (p1 === 'paper' && p2 === 'rock')) {
+        return `Player 1 won!`;
+    } else if ((p1 === 'scissors' && p2 === 'rock')
+        || (p1 === 'paper' && p2 === 'scissors')
+        || (p1 === 'rock' && p2 === 'paper')) {
+        return `Player 2 won!`;
+
+    } else {
+        return 'Draw!';
+    }
+};
+// kitas budas
+/*const rps = (p1, p2) => {
+  const rules = {
+    'scissors': 'paper',
+    'rock': 'scissors',
+    'paper': 'rock'
+  };
+  return rules[p1] === p2 ? 'Player 1 won!' : p1 === p2 ? 'Draw!' : 'Player 2 won!';
+}; */
+console.log(rps('rock', 'scissors'), 1);
+console.log(rps('scissors', 'paper'), 1);
+console.log(rps('paper', 'rock'), 1);
+console.log(rps('scissors', 'rock'), 2);
+console.log(rps('paper', 'scissors'), 2);
+console.log(rps('rock', 'paper'), 2);
+console.log(rps('rock', 'rock'), 'Draw!');
+console.log(rps('scissors', 'scissors'), 'Draw!');
+console.log(rps('paper', 'paper'), 'Draw!');
+console.log(`\n 195 uzdv.............`);
