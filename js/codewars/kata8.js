a8b3555de38454103122d47e893a6ed7c53abd2f
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-08-14 08:45:38 +0300

    kata8

--- js/codewars/kata8.js
+++ js/codewars/kata8.js
@@ -219,3 +219,40 @@ function combineNames(firstName, lastName) {
 }
 console.log(combineNames('James', 'Stevens'));
 console.log(`\n 193 uzdv...............`);
+
+// https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
+function likes(names) {
+    for (let i = 0; i < names.length; i++) {
+        if (names.length === 1) {
+            return `${names[0]} likes this`;
+        } else if (names.length === 2) {
+            return `${names[0]} and ${names[1]} like this`;
+        } else if (names.length === 3) {
+            return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+        } else if (names.length >= 4) {
+            return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+        }
+    }
+    return 'no one likes this';
+}
+// kitas budas
+/*function likes(names) {
+  switch(names.length) {
+    case 0:
+      return 'no one likes this';
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+} */
+console.log(likes([]));
+console.log(likes(['Peter']));
+console.log(likes(['Jacob', 'Alex']));
+console.log(likes(['Max', 'John', 'Mark']));
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']));
+console.log(`\n 194 uzdv...............`);
