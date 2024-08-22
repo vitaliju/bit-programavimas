6aa711929e6bb0f96a517d209a19870c22b9dfbb
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-08-22 13:57:56 +0300

    kata9 file update

--- js/codewars/kata9.js
+++ js/codewars/kata9.js
@@ -152,3 +152,40 @@ console.log('a'.digit(), false);
 console.log('a5'.digit(), false);
 console.log('14'.digit(), false);
 console.log(`\n 210 uzdv..............`);
+
+// https://www.codewars.com/kata/56a3f08aa9a6cc9b75000023/train/javascript
+function validateUsr(username) {
+    return username.length >= 4
+        && username.length <= 16
+        && username.toLowerCase() === username
+        && username.match(/[a-z]|_|\d/g).length === username.length;
+}
+//kitas budas
+/*function validateUsr(username) {
+  
+    // - `^`        Start from the beginning of the string.
+    // - `[]`       Allow any character specified, including...
+    // - `a-z`      anything from a to z,
+    // - `0-9`      anything from 0 to 9,
+    // - `_`        and underscore.
+    // - `{4,16}`   Accept 4 to 16 of allowed characters, both numbers included.
+    // - `$`        End the string right after specified amount of allowed characters is given.
+  
+const validator = /^[a-z0-9_]{4,16}$/;
+
+return validator.test(username);
+} */
+console.log(validateUsr('asddsa'), true);
+console.log(validateUsr('a'), false);
+console.log(validateUsr('Hass'), false);
+console.log(validateUsr('Hasd_12assssssasasasasasaasasasasas'), false);
+console.log(validateUsr(''), false);
+console.log(validateUsr('____'), true);
+console.log(validateUsr('012'), false);
+console.log(validateUsr('0123'), true);
+console.log(validateUsr('1234567890abcdefg'), false);
+console.log(validateUsr('p1pp1'), true);
+console.log(validateUsr('asd43 34'), false);
+console.log(validateUsr('asd43_34'), true);
+console.log(validateUsr('abcd'), true);
+console.log(`\n 211 uzdv..............`);
